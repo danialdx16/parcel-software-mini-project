800bf5c99a13a4b25214afb4290632f8329d94d5
Danial Haslisham <danialwingform@gmail.com>
2021-07-31 21:11:36 +0800

    Create monthly database
    Created tuple for parcel database (For loop)

--- parceldatabase.py
+++ parceldatabase.py
@@ -1,26 +1,32 @@
-from tkinter import *
 import sqlite3
 
-root = Tk()
-root.title('parceldatabase')
-root.iconbitmap('')
-root.iconbitmap('D:\SEM 2\VGT123\computer programming\labmodule')
-root.geometry("400x400")
+month = [
+        'Jan',
+        'Feb',
+        'Mac',
+        'Apr',
+        'May',
+        'Jun',
+        'Jul',
+        'Aug',
+        'Spt',
+        'Oct',
+        'Nov',
+        'Dec'
+        ]
 
+for x in range(len(month)):
+    con = sqlite3.connect(f'parcel_system_{month[x]}.db')
+    c = con.cursor()
+    
+    c.execute("""CREATE TABLE parcel_system(
+        name text, 
+        phone integer,
+        trackno integer,
+        rack integer,
+        date integer,
+        status
+        )""")
 
-
-con = sqlite3.connect('parcel_system.db')
-c = con.cursor()
- 
-c.execute("""CREATE TABLE parcel_system(
-    name text, 
-    phone integer,
-    trackno integer,
-    rack integer,
-    date integer,
-    status
-    )""")
-
-con.commit()
-con.close() 
-root.mainloop()
+    con.commit()
+    con.close() 
